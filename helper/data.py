42daa70b601df8682338eff5ab26eb547b57fe9d
Sanket Patil <psanket18052001@gmail.com>
2022-03-23 18:41:03 +0530

    updated

---
+++ helper/data.py
@@ -0,0 +1,55 @@
+data = {
+    "services":{
+        "7219442303":{
+            "living_room":{
+                "room_name":"livig_room",
+                "devices":{
+                    "light":False,
+                    "fan":False,
+                    "tv":False,
+                    "iron":False
+                }
+            },
+            "bedroom":{
+                "room_name":"bedroom",
+                "devices":{
+                    "light":False,
+                    "fan":False,
+                    "tv":False,
+                    "iron":False
+                }
+            },
+            "kitchen":{
+               "room_name":"kitchen",
+                "devices":{
+                    "light":False,
+                    "fan":False,
+                    "tv":False,
+                    "iron":False
+                }
+            }
+        }
+                                
+
+    },
+    "auth":{
+        "patil.sanket18@outlook.com":{
+            "email":"patil.sanket18@outlook.com",
+            "passwd":"Sanket@123",
+            "uid":"7219442303"
+        },
+        
+    },
+    "user":{
+        "7219442303":{
+            "email":"patil.sanket18@outlook.com",
+            "fname":"Sanket",
+            "lname":"Patil",
+            "profile_img":"http://this.is.pic"
+            
+            },
+
+        },
+        
+    }
+
